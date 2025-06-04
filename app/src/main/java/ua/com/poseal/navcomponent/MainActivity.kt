856e68ff5b08f2769c27628d2a3f31941e692a8a
Surftrader <poseal@gmail.com>
2025-06-04 18:11:48 +0300

    Navigation Component: Scaffold, TopAppBar

--- app/src/main/java/ua/com/poseal/navcomponent/MainActivity.kt
+++ app/src/main/java/ua/com/poseal/navcomponent/MainActivity.kt
@@ -5,20 +5,30 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import dagger.hilt.android.AndroidEntryPoint
 import ua.com.poseal.navcomponent.model.ItemsRepository
 import ua.com.poseal.navcomponent.screens.AddItemRoute
+import ua.com.poseal.navcomponent.screens.AppToolbar
 import ua.com.poseal.navcomponent.screens.ItemsRoute
 import ua.com.poseal.navcomponent.screens.LocalNavController
+import ua.com.poseal.navcomponent.screens.NavigateUpAction
 import ua.com.poseal.navcomponent.screens.add.AddItemScreen
 import ua.com.poseal.navcomponent.screens.items.ItemScreen
 import ua.com.poseal.navcomponent.ui.theme.NavigationComponentTheme
@@ -49,16 +59,46 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun NavApp() {
     val navController = rememberNavController()
-    CompositionLocalProvider(
-        LocalNavController provides navController
-    ) {
-        NavHost(
-            navController = navController,
-            startDestination = ItemsRoute,
-            modifier = Modifier.fillMaxSize(),
+    val currentBackStackEntry by navController.currentBackStackEntryAsState()
+    val titleRes = when (currentBackStackEntry?.destination?.route) {
+        ItemsRoute -> R.string.items_screen
+        AddItemRoute -> R.string.add_item_screen
+        else -> R.string.app_name
+    }
+    Scaffold(
+        topBar = {
+            AppToolbar(
+                navigateUpAction = if (navController.previousBackStackEntry == null) {
+                    NavigateUpAction.Hidden
+                } else {
+                    NavigateUpAction.Visible(
+                        onClick = { navController.navigateUp() }
+                    )
+                },
+                titleRes = titleRes,
+            )
+        },
+        floatingActionButton = {
+            if (currentBackStackEntry?.destination?.route == ItemsRoute) {
+                FloatingActionButton(
+                    onClick = { navController.navigate(AddItemRoute) },
+                ) {
+                    Icon(imageVector = Icons.Default.Add, contentDescription = null)
+                }
+            }
+        }
+    ) { paddingValues ->
+        CompositionLocalProvider(
+            LocalNavController provides navController
         ) {
-            composable(ItemsRoute) { ItemScreen() }
-            composable(AddItemRoute) { AddItemScreen() }
+            NavHost(
+                navController = navController,
+                startDestination = ItemsRoute,
+                modifier = Modifier.fillMaxSize().padding(paddingValues),
+            ) {
+                composable(ItemsRoute) { ItemScreen() }
+                composable(AddItemRoute) { AddItemScreen() }
+            }
         }
     }
 }
--- app/src/main/java/ua/com/poseal/navcomponent/screens/items/ItemsScreen.kt
+++ app/src/main/java/ua/com/poseal/navcomponent/screens/items/ItemsScreen.kt
@@ -5,11 +5,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.FloatingActionButton
-import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -18,25 +14,20 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
-import ua.com.poseal.navcomponent.screens.AddItemRoute
-import ua.com.poseal.navcomponent.screens.LocalNavController
 import ua.com.poseal.navcomponent.screens.items.ItemsViewModel.ScreenState
 
 @Composable
 fun ItemScreen() {
-    val navController = LocalNavController.current
     val viewModel: ItemsViewModel = hiltViewModel()
     val screenState = viewModel.stateFlow.collectAsState()
     ItemsContent(
         getScreenState = { screenState.value },
-        onLaunchAddItemScreen = { navController.navigate(AddItemRoute) }
     )
 }
 
 @Composable
 fun ItemsContent(
     getScreenState: () -> ScreenState,
-    onLaunchAddItemScreen: () -> Unit,
 ) {
     Box(
         modifier = Modifier.fillMaxSize(),
@@ -58,14 +49,6 @@ fun ItemsContent(
                 }
             }
         }
-        FloatingActionButton(
-            onClick = onLaunchAddItemScreen,
-            modifier = Modifier
-                .align(Alignment.BottomEnd)
-                .padding(bottom = 16.dp, end = 16.dp)
-        ) {
-            Icon(imageVector = Icons.Default.Add, contentDescription = null)
-        }
     }
 }
 
@@ -74,6 +57,5 @@ fun ItemsContent(
 private fun ItemScreenPreview() {
     ItemsContent (
         getScreenState = { ScreenState.Loading },
-        onLaunchAddItemScreen = {},
     )
 }
